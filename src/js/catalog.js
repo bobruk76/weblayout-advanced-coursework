112c85a3e2966ed59d57b4b33b7d43cc22ffabd5
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-05 18:09:04 +0300

    fix grid styles

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -3,11 +3,15 @@ import Swiper, {Navigation} from 'swiper'
 const swiperCatalog = new Swiper('.catalog__slider', {
   modules: [Navigation],
 
-  effect: 'fade',
+  // effect: 'fade',
+  // slidesPerView: 3,
+  // grid: {
+  //   rows: 3,
+  // },
   slidesPerView: 3,
-  grid: {
-    rows: 3,
-  },
+  slidesPerColumn: 3,
+  slidesPerGroup :3,
+
   spaceBetween: 32,
 
   // navigation: {
