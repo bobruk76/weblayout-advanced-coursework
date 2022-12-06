6b89d84e4ed76d681981f1e1e481b78fa3941070
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-06 14:43:43 +0300

    fix catalog slider

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -3,22 +3,12 @@ import Swiper, {Navigation} from 'swiper'
 const swiperCatalog = new Swiper('.catalog__slider', {
   modules: [Navigation],
 
-  // effect: 'fade',
-  // slidesPerView: 3,
+  effect: 'fade',
+  slidesPerView: 3,
   // grid: {
   //   rows: 3,
   // },
-  slidesPerView: 3,
-  slidesPerColumn: 3,
-  slidesPerGroup :3,
 
   spaceBetween: 32,
-
-  // navigation: {
-  //   prevEl: '.useful__navigation-prev',
-  //   nextEl: '.useful__navigation-next',
-  // },
-  // autoplayDisableOnInteraction: false,
-  direction: 'horizontal',
   a11y: true,
 })
