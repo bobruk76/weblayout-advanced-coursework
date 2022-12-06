5633d190524075cb44f38f9f37120a58c631ccf6
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-06 16:51:08 +0300

    fix catalog slider

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,14 +1,22 @@
-import Swiper, {Navigation} from 'swiper'
+import Swiper, {Pagination, Navigation} from 'swiper'
 
 const swiperCatalog = new Swiper('.catalog__slider', {
-  modules: [Navigation],
+  modules: [Pagination, Navigation],
 
   effect: 'fade',
   slidesPerView: 3,
+  slidesPerColumn: 3,
+  slidesPerGroup :3,
+  spaceBetween: 32,
+
+  pagination: {
+    el: ".catalog__slider-pagination",
+    clickable: true,
+  },
+
   // grid: {
   //   rows: 3,
   // },
 
-  spaceBetween: 32,
   a11y: true,
 })
