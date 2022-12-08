af47918b74054c25ebb616cda2e7d13a843a5d3c
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-08 17:40:50 +0300

    fix grid options

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -4,19 +4,18 @@ const swiperCatalog = new Swiper('.catalog__slider', {
   modules: [Pagination, Navigation],
 
   effect: 'fade',
+
   slidesPerView: 3,
-  slidesPerColumn: 3,
-  slidesPerGroup :3,
+  grid: {
+    rows: 3,
+  },
+
   spaceBetween: 32,
 
   pagination: {
     el: ".catalog__slider-pagination",
     clickable: true,
   },
-
-  // grid: {
-  //   rows: 3,
-  // },
 
   a11y: true,
 })
