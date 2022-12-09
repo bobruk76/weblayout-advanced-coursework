dcd5755718f16ffef69ae4e1440bac134e9090a0
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-09 17:59:15 +0300

    add pagination

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -3,9 +3,8 @@ import Swiper, {Pagination, Navigation, Grid} from 'swiper'
 const swiperCatalog = new Swiper('.catalog__slider', {
   modules: [Pagination, Navigation, Grid],
 
-  effect: 'fade',
-
   slidesPerView: 3,
+  slidesPerGroup: 3,
   grid: {
     rows: 3,
   },
@@ -15,6 +14,9 @@ const swiperCatalog = new Swiper('.catalog__slider', {
   pagination: {
     el: ".catalog__slider-pagination",
     clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   },
 
   a11y: true,
