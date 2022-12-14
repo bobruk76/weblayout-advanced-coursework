d92f778c9d6460c2885320f4c6832830f90868e2
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-14 14:40:03 +0300

    add multi-range-slider acripts

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -21,3 +21,31 @@ const swiperCatalog = new Swiper('.catalog__slider', {
 
   a11y: true,
 })
+
+const inputLeft = document.getElementById("input-left");
+const inputRight = document.getElementById("input-right");
+
+const thumbLeft = document.querySelector(".slider > .thumb.left");
+const thumbRight = document.querySelector(".slider > .thumb.right");
+const range = document.querySelector(".slider > .range");
+
+const setLeftValue = () => {
+  const _this = inputLeft;
+  const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+  _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  const percent = ((_this.value - min) / (max - min)) * 100;
+  thumbLeft.style.left = percent + "%";
+  range.style.left = percent + "%";
+};
+
+const setRightValue = () => {
+  const _this = inputRight;
+  const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
+  _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  const percent = ((_this.value - min) / (max - min)) * 100;
+  thumbRight.style.right = 100 - percent + "%";
+  range.style.right = 100 - percent + "%";
+};
+
+inputLeft.addEventListener("input", setLeftValue);
+inputRight.addEventListener("input", setRightValue);
