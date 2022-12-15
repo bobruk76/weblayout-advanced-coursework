291fbbcb117bbdf3548c167049201a4c9250627b
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2022-12-15 15:33:33 +0300

    fix script

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -22,6 +22,9 @@ const swiperCatalog = new Swiper('.catalog__slider', {
   a11y: true,
 })
 
+const inputLeft = document.querySelector(".multi-range-slider__input-number--left");
+const inputRight = document.querySelector(".multi-range-slider__input-number--right");
+
 const inputRangeLeft = document.querySelector(".multi-range-slider__input-range--left");
 const inputRangeRight = document.querySelector(".multi-range-slider__input-range--right");
 
@@ -32,8 +35,9 @@ const range = document.querySelector(".multi-range > .range");
 const setLeftValue = () => {
   const _this = inputRangeLeft;
   const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
-  console.log(min, max)
   _this.value = Math.min(parseInt(_this.value), parseInt(inputRangeRight.value));
+
+  inputLeft.value = _this.value;
   const percent = ((_this.value - min) / (max - min)) * 100;
   thumbLeft.style.left = percent + "%";
   range.style.left = percent + "%";
@@ -43,6 +47,8 @@ const setRightValue = () => {
   const _this = inputRangeRight;
   const [min, max] = [parseInt(_this.min), parseInt(_this.max)];
   _this.value = Math.max(parseInt(_this.value), parseInt(inputRangeLeft.value));
+
+  inputRight.value = _this.value;
   const percent = ((_this.value - min) / (max - min)) * 100;
   thumbRight.style.right = 100 - percent + "%";
   range.style.right = 100 - percent + "%";
