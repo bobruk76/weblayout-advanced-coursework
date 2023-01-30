63102fa6238e7c46f5d8c8ee0ba33fdfc1ae19de
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2023-01-30 17:34:30 +0300

    add slider to product page

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,19 @@
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-const productId = urlParams.get('id')
-console.log(productId)
+import Swiper, {Navigation} from 'swiper'
+
+const swiperLikeProducts = new Swiper('.like-products__slider', {
+  modules: [Navigation],
+
+  effect: 'fade',
+  slidesPerView: "auto",
+  slidesPerGroup: 3,
+
+  navigation: {
+    prevEl: ".like-products__navigation-prev",
+    nextEl: ".like-products__navigation-next",
+  },
+
+  autoplayDisableOnInteraction: false,
+  direction: 'horizontal',
+
+  a11y: true,
+})
