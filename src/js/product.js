f1eb5b5bca9c5045818b7df42a50c4ce1aa956d1
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2023-02-02 12:24:12 +0300

    add modal window component style

--- src/js/product.js
+++ src/js/product.js
@@ -18,9 +18,18 @@ const swiperLikeProducts = new Swiper('.like-products__slider', {
   a11y: true,
 })
 
-
 const buyButton = document.querySelector('#productBuyOneClick')
 const productCardDialog = document.querySelector('#productCardDialog')
+
+const modalWindows = document.querySelectorAll('.modal-window')
+const closeModalButtons = document.querySelectorAll('.modal-window__btn-close')
+closeModalButtons.forEach(item => {
+  item.addEventListener('click', e => {
+    modalWindows.forEach(modal => {
+      modal.classList.remove("modal-window--visible")
+    })
+  })
+})
 
 buyButton.addEventListener('click', () => {
   productCardDialog.classList.add("modal-window--visible")
